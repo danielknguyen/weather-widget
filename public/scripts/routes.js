f4223484834d11adc78f79d69e8874add6001e8b
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-02-02 19:22:34 -0800

    added posting route for searching

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,6 +1,23 @@
-var routes = module.exports = function(app) {
+var routes = module.exports = function(app, weather) {
 
-  app.get('/', function (req, res){
+  app.get('/', function (req, res) {
     res.render('index.html');
   });
-}
+
+  app.post('/', function (req, res) {
+    
+    // console.log('This is my search: ' + req.body.query.location);
+    // location={city name},{country code}
+    var location = req.body.query.location;
+
+    // set city name
+    weather.setCity(location);
+
+    // get all weather data from set city
+    weather.getAllWeather(function (err, data) {
+      console.log(data);
+    });
+
+    res.render('index.html');
+  });
+};
