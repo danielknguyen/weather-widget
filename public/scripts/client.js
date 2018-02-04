8823f3ae1b51a5f86f275a9cec714a5825d05543
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-02-03 20:32:59 -0800

    added celsius switch to farenheit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,17 @@
+var celsius = false;
+
 $(document).ready(function () {
 
+  $('.deg').on('click', function() {
+    if (!celsius) {
+      celsius = true;
+      $('.celsius').css('display', 'block');
+      $('.fahrenheit').css('display', 'none');
+    } else {
+      celsius = false;
+      $('.fahrenheit').css('display', 'block');
+      $('.celsius').css('display', 'none');
+    }
+  });
+  
 });
