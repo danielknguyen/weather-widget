5b556ce4ffb2468ff377c001ae884bcd2f1d4002
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-02-03 20:33:37 -0800

    updated post request for query city

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -12,11 +12,10 @@ var routes = module.exports = function(app, weather, date) {
 
       if (err) {
         console.log(err);
-         res.redirect("/");
+        res.redirect("/");
       }
 
       // console.log("weather forecast: " + data);
-
       data.list.forEach(function(element) {
         // multiple epoch time(seconds) by 1000 to convert to javascript time(milliseconds)
         var epochToJsDate = new Date(element.dt * 1000);
@@ -28,15 +27,21 @@ var routes = module.exports = function(app, weather, date) {
         element.dt = date_format;
         // reassign object image icon to full url
         element.weather[0].icon = imageUrl;
+        // round celsius
+        element.temp.day = Math.ceil(element.temp.day);
+        // set fahrenheit
+        fahrenheit = Math.ceil(element.temp.day * 9/5 + 32);
+        element.temp.fahrenheit = fahrenheit;
 
         // console.log(element);
       });
 
       obj.data = data;
 
-      // console.log(obj);
+
+      // console.log("this is the data " + obj);
       res.render('index.html', data=obj);
-    })
+    });
   });
 
   app.post('/', function (req, res) {
@@ -56,33 +61,46 @@ var routes = module.exports = function(app, weather, date) {
     // get 3 days forecast
     weather.getWeatherForecastForDays(4, function(err, data) {
 
+      var loopSwitch = true;
+
       if (err) {
         console.log(err);
-         res.redirect("/");
+        res.redirect("/");
       }
 
-      // console.log("weather forecast: " + data);
+      // console.log("weather forecast: " + JSON.stringify(data));
 
-      data.list.forEach(function(element) {
-        // multiple epoch time(seconds) by 1000 to convert to javascript time(milliseconds)
-        var epochToJsDate = new Date(element.dt * 1000);
-        // format time to display day of the week
-        var date_format = date.format(epochToJsDate,'ddd')
-        // setup image url
-        var imageUrl = "http://openweathermap.org/img/w/" + element.weather[0].icon + ".png";
-        // reassign object datetime to display only the day of the week
-        element.dt = date_format;
-        // reassign object image icon to full url
-        element.weather[0].icon = imageUrl;
+      // check if city was found
+      if (data.cod === 404 || data.message === "city not found") {
+        console.log("city not found");
+        res.redirect("/");
+      } else {
+        data.list.forEach(function(element) {
+          // multiple epoch time(seconds) by 1000 to convert to javascript time(milliseconds)
+          var epochToJsDate = new Date(element.dt * 1000);
+          // format time to display day of the week
+          var date_format = date.format(epochToJsDate,'ddd')
+          // setup image url
+          var imageUrl = "http://openweathermap.org/img/w/" + element.weather[0].icon + ".png";
+          // reassign object datetime to display only the day of the week
+          element.dt = date_format;
+          // reassign object image icon to full url
+          element.weather[0].icon = imageUrl;
+          // round celsius
+          element.temp.day = Math.ceil(element.temp.day);
+          // set fahrenheit
+          fahrenheit = Math.ceil(element.temp.day * 9/5 + 32);
+          element.temp.fahrenheit = fahrenheit;
 
-        // console.log(element);
-      });
+          // console.log(element);
+        });
 
-      obj.data = data;
+        obj.data = data;
 
-      // console.log(obj);
-      res.render('index.html', data=obj);
-    })
+        // console.log("this is the data " + obj);
+        res.render('index.html', data=obj);
+      };
+    });
   });
 
 };
