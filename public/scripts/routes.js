bef857ea11e3b8ff3d199f61b0cf60d4337fd065
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-02-03 16:53:33 -0800

    integrated api into clientside

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,23 +1,88 @@
-var routes = module.exports = function(app, weather) {
-
+var routes = module.exports = function(app, weather, date) {
   app.get('/', function (req, res) {
-    res.render('index.html');
+
+    // weather data will be pushed into this obj. Render template with obj
+    var obj = {};
+
+    // default san francisco
+    weather.setCityId(5391959);
+
+    // get 3 days forecast
+    weather.getWeatherForecastForDays(4, function(err, data) {
+
+      if (err) {
+        console.log(err);
+         res.redirect("/");
+      }
+
+      // console.log("weather forecast: " + data);
+
+      data.list.forEach(function(element) {
+        // multiple epoch time(seconds) by 1000 to convert to javascript time(milliseconds)
+        var epochToJsDate = new Date(element.dt * 1000);
+        // format time to display day of the week
+        var date_format = date.format(epochToJsDate,'ddd')
+        // setup image url
+        var imageUrl = "http://openweathermap.org/img/w/" + element.weather[0].icon + ".png";
+        // reassign object datetime to display only the day of the week
+        element.dt = date_format;
+        // reassign object image icon to full url
+        element.weather[0].icon = imageUrl;
+
+        // console.log(element);
+      });
+
+      obj.data = data;
+
+      // console.log(obj);
+      res.render('index.html', data=obj);
+    })
   });
 
   app.post('/', function (req, res) {
-    
-    // console.log('This is my search: ' + req.body.query.location);
+
+    // weather data will be pushed into this obj. Render template with obj
+    var obj = {};
+
     // location={city name},{country code}
     var location = req.body.query.location;
+
+    // reset cityId
+    weather.setCityId();
 
     // set city name
     weather.setCity(location);
 
-    // get all weather data from set city
-    weather.getAllWeather(function (err, data) {
-      console.log(data);
-    });
+    // get 3 days forecast
+    weather.getWeatherForecastForDays(4, function(err, data) {
 
-    res.render('index.html');
+      if (err) {
+        console.log(err);
+         res.redirect("/");
+      }
+
+      // console.log("weather forecast: " + data);
+
+      data.list.forEach(function(element) {
+        // multiple epoch time(seconds) by 1000 to convert to javascript time(milliseconds)
+        var epochToJsDate = new Date(element.dt * 1000);
+        // format time to display day of the week
+        var date_format = date.format(epochToJsDate,'ddd')
+        // setup image url
+        var imageUrl = "http://openweathermap.org/img/w/" + element.weather[0].icon + ".png";
+        // reassign object datetime to display only the day of the week
+        element.dt = date_format;
+        // reassign object image icon to full url
+        element.weather[0].icon = imageUrl;
+
+        // console.log(element);
+      });
+
+      obj.data = data;
+
+      // console.log(obj);
+      res.render('index.html', data=obj);
+    })
   });
+
 };
